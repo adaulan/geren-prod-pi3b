0c243bb587cc6c725a749aa44185435bd6d7913d
adaulan.eogomes <adaulan.eogomes@CAS511923W1031.senacsp.edu.br>
2019-02-28 21:50:38 -0300

    Alteração da loja

--- geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/Principal.java
+++ geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/Principal.java
@@ -12,5 +12,7 @@ package br.senac.tads.pi3.gerenprod;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Teste Maven");
+        int i = 0;
+        System.out.println("Valor de i++" + i++);
     }
 }
